6ddb2528d6a01f81c815e09614b605760368797a
Aleksandr Levchuk <alevchuk@gmail.com>
2011-09-28 14:00:21 -0700

    Automtic commit

--- trembl/Home.md
+++
@@ -1,1 +0,0 @@
-Welcome to the uniprot-stats-history wiki!
--- trembl/www.ebi.ac.uk/inc/js/trim.js
+++
@@ -1,8 +0,0 @@
-// from http://blog.stevenlevithan.com/archives/faster-trim-javascript
-function trim12 (str) {
-	var	str = str.replace(/^\s\s*/, ''),
-		ws = /\s/,
-		i = str.length;
-	while (ws.test(str.charAt(--i)));
-	return str.slice(0, i + 1);
-}
