0c0955bc90d67272409f9bcc7da8e6dd246ad18a
Aleksandr Levchuk <alevchuk@gmail.com>
2011-09-30 14:38:44 -0700

    Parse out the version number from HTML
    A separate directory is created for every version

---
+++ get-release-rename-and-index.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+(set -e; set -u;
+  sp_file="swiss-prot/web.expasy.org/docs/relnotes/relstat.html"
+  line=$(grep "protein knowledgebase release" "$sp_file" | head -1)
+  sp_release=$(expr "$line" : '.*release \(.*\) statistics')
+  echo "$sp_release"
+  sp_dir="$sp_release-swiss-prot"
+  mkdir "$sp_dir" 2> /dev/null && true
+  mv "swiss-prot/"* "$sp_dir/")
+
+(set -e; set -u;
+  tr_file="trembl/www.ebi.ac.uk/uniprot/TrEMBLstats/relstat.html"
+  line=$(grep "PROTEIN DATABASE RELEASE" "$tr_file" | head -1)
+  tr_release=$(expr "$line" : '.*RELEASE \(.*\) STATISTICS')
+  echo "$tr_release"
+  tr_dir="$tr_release-trembl"
+  mkdir "$tr_dir" 2> /dev/null && true
+  mv "trembl/"* "$tr_dir/")
+
+
+set -e; set -u;
+(
+  for f in *"/index.html"; do
+    echo "<p><a href=\"$f\">$f</a></p>"
+  done
+  
+  for f in *"/www.ebi.ac.uk/uniprot/TrEMBLstats/relstat.html"; do
+    echo "<p><a href=\"$f\">$f</a></p>"
+  done
+) | sort > index.html-tmp
+mv index.html{-tmp,}
--- mirror-commit-push.sh
+++ mirror-commit-push.sh
@@ -6,15 +6,19 @@ cd $SCRIPT_DIR
 SWISSPROT_URL=http://web.expasy.org/docs/relnotes/relstat.html
 TREMBL_URL=http://www.ebi.ac.uk/uniprot/TrEMBLstats/relstat.html
 
+mkdir swiss-prot 2> /dev/null
 ( set -e
   set -u
-  cd swiss-prot/
+  cd swiss-prot
   wget        --quiet -E -H -k -K -p $SWISSPROT_URL )
 
+mkdir trembl 2> /dev/null
 ( set -e
   set -u
-  cd trembl/
+  cd trembl
   wget        --quiet -E -H -k -K -p $TREMBL_URL )
+
+./get-release-rename-and-index.sh
 
 git add .
 git commit    --quiet -a -m "Automatic commit" 
