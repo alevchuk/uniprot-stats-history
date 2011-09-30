2caea93c063ad28a9fab7f7394bfc091d9f8af41
Aleksandr Levchuk <alevchuk@gmail.com>
2011-09-30 14:59:03 -0700

    No chatting if everything is OK

--- get-release-rename-and-index.sh
+++ get-release-rename-and-index.sh
@@ -4,7 +4,7 @@
   sp_file="swiss-prot/web.expasy.org/docs/relnotes/relstat.html"
   line=$(grep "protein knowledgebase release" "$sp_file" | head -1)
   sp_release=$(expr "$line" : '.*release \(.*\) statistics')
-  echo "$sp_release"
+  #echo "$sp_release"
   sp_dir="$sp_release-swiss-prot"
   mkdir "$sp_dir" 2> /dev/null && true
   mv --no-clobber "swiss-prot/"* "$sp_dir/")
@@ -13,7 +13,7 @@
   tr_file="trembl/www.ebi.ac.uk/uniprot/TrEMBLstats/relstat.html"
   line=$(grep "PROTEIN DATABASE RELEASE" "$tr_file" | head -1)
   tr_release=$(expr "$line" : '.*RELEASE \(.*\) STATISTICS')
-  echo "$tr_release"
+  #echo "$tr_release"
   tr_dir="$tr_release-trembl"
   mkdir "$tr_dir" 2> /dev/null && true
   mv --no-clobber "trembl/"* "$tr_dir/")
